9353fc738be9e81a35ff213de1d9d22ef1fcc6ec
JustsayNini <nissilossala@gmail.com>
2026-05-07 09:29:03 +0200

    Finished my calculator AT LASTgit add .!

---
+++ index.js
@@ -0,0 +1,20 @@
+const displayScreen= document.getElementById("screen-box");
+
+function display(input){
+    displayScreen.value += input
+}
+
+function calculate(){
+    try{
+        displayScreen.value = eval(displayScreen.value)
+    }
+
+    catch(error){
+        displayScreen.value ="Error, try again"
+    }
+    
+}
+
+function clearDisplay(){
+    displayScreen.value = ""
+}
