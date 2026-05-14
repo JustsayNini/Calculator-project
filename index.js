a6c0a0dfbe33785a793e33883d51f13d26e2ba08
JustsayNini <nissilossala@gmail.com>
2026-05-14 06:11:22 +0200

    Added more buttons, better logic, and sacrificed my sanity and sleep for this project.

--- index.js
+++ index.js
@@ -5,16 +5,63 @@ function display(input){
 }
 
 function calculate(){
+    const inputParts=displayScreen.value.split(/(\^2|[+\-*/√^]+)/);
+    let total = parseFloat(inputParts[0]);
 
-    try{
-        
-        displayScreen.value = eval(displayScreen.value)
+    for (i=1; i<inputParts.length; i+=2){
+
+        const operator = inputParts[i]
+        const equationNumber = parseFloat(inputParts[i+1])
+
+        if (operator === '+') {
+
+            total += equationNumber
+            
+        }else if (operator === '-') {
+
+            total -= equationNumber
+            
+        } else if (operator === '*') {
+
+            total *= equationNumber
+            
+        } else if (operator === '/') {
+
+            if(equationNumber === 0){
+
+                return displayScreen.value="Error!";
+                
+            } else {
+
+                total /= equationNumber
+
+            }
+        } else if(operator === '^2') {
+
+            total = Math.pow(total, 2);
+            i -= 1;
+
+        } else if(operator === '^'){
+
+            total = Math.pow(total, equationNumber)
+
+        } else if (operator === '√'){
+
+            if(isNaN(total)){
+                total = Math.sqrt(equationNumber);
+            } else {
+                total *= Math.sqrt(equationNumber);
+            }
+
+        } else {
+
+            "Please enter valid input."
+
+        }
     }
 
-    catch(error){
-        displayScreen.value ="Error, try again"
-    }
-    
+    displayScreen.value = total
+
 }
 
 
